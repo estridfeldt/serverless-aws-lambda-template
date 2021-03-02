7d0f015ca443d4bd02372f19e49beaf061a197e0
emst <emil.stridfeldt@gmail.com>
2021-03-02 20:39:41 +0100

    New name

--- serverless.ts
+++ serverless.ts
@@ -3,7 +3,7 @@ import type { AWS } from '@serverless/typescript';
 import hello from '@functions/hello';
 
 const serverlessConfiguration: AWS = {
-  service: 'backend',
+  service: 'backend-antagning',
   frameworkVersion: '2',
   custom: {
     webpack: {
