134830df20ccec8770c5a4d8670ab19b8a20ab7f
emst <emil.stridfeldt@gmail.com>
2021-03-06 22:19:07 +0100

    Sample e2e test

--- e2e/test.spec.ts
+++ e2e/test.spec.ts
@@ -1,5 +1,32 @@
+import AWS from 'aws-sdk';
+import axios from 'axios';
+
+const cloudformation = new AWS.CloudFormation({
+  apiVersion: '2010-05-15',
+  region: 'eu-north-1',
+});
+
+const apiEndpoint = (stackName: string): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    cloudformation.describeStacks({ StackName: stackName }, function (err, data) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data.Stacks[0].Outputs.filter((output) => output.OutputKey === 'ServiceEndpoint')[0].OutputValue);
+      }
+    });
+  });
+};
+
+const postHello = (endpoint: string) =>
+  axios.post(`${endpoint}/hello`, { name: 'Test' }).then((response) => response.data);
+
 describe('test', () => {
   it('should test e2e', () => {
-    expect(1).toEqual(1);
+    return apiEndpoint('backend-antagning-dev').then((endpoint) => {
+      return postHello(endpoint).then((response) => {
+        expect(response.message).toEqual('Hello Test, welcome to the exciting Serverless world!');
+      });
+    });
   });
 });
