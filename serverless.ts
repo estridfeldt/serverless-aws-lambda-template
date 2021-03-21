7b147c018c972147aeb0f5392fcde79196b150d5
emst <emil.stridfeldt@gmail.com>
2021-03-21 16:47:28 +0100

    Generic service name

--- serverless.ts
+++ serverless.ts
@@ -3,7 +3,7 @@ import type { AWS } from '@serverless/typescript';
 import hello from '@functions/hello';
 
 const serverlessConfiguration: AWS = {
-  service: 'backend-antagning',
+  service: 'sample-service',
   frameworkVersion: '2',
   custom: {
     webpack: {
